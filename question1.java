bff1d7c96fb143fb509873949b6320d995ddc113
Gurmit Singh <129723655+Gurmit147@users.noreply.github.com>
2024-12-19 12:37:11 +0800

    Add files via upload

---
+++ question1.java
@@ -0,0 +1,10 @@
+public class question1 {
+    public static void main(String[] args) {
+     String car[] = {"Honda","Toyota","Proton Waja","BMW"};
+
+     for(int i=0; i<car.length; i++) {
+        System.out.println(car[i]);
+     }
+    
+    }
+}
